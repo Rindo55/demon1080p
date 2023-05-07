8499fa58de43789ddf4c24b821a15bdc44f5bccd
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-05-07 23:47:06 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -171,11 +171,11 @@ async def start_uploading(data):
         subtitle = subtitle.replace("UKR", "Ukranian")
         DEMON_ID = 29755
         sourcetext =  f"**#Encoded_File**" + "\n" + f"**‣ File Name**: `{razo}`" + "\n" + "**‣ Video**: `1080p HEVC x265 10Bit`" + "\n" + "**‣ Audio**: `Japanese`" + "\n" + f"**‣ Subtitle**: `{subtitle}`"
-        await app.send_message(
-                    chat_id=KAYO_ID,
-                    text=sourcetext,
-                    reply_to_message_id=DEMON_ID
-                   )
+        untext = await app.send_message(
+                             chat_id=KAYO_ID,
+                             text=sourcetext,
+                             reply_to_message_id=DEMON_ID
+                             )
         await asyncio.sleep(2)
         await app.send_sticker(KAYO_ID,"CAACAgUAAxkBAAEU_9FkRrLoli952oqIMVFPftW12xYLRwACGgADQ3PJEsT69_t2KrvBLwQ")
         os.rename(file,"video.mkv")
